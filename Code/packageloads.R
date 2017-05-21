0b6de8a115d72f5480fecd5e44e67caf09ecea63
Pepito3 <maic_pasquier@hotmail.com>
2017-05-21 09:44:30 +0200

    update maik code

--- Code/packageloads.R
+++ Code/packageloads.R
@@ -7,3 +7,8 @@ c("tidyr", "dplyr" ,"readr", "tibble", "Dplyr2Xts","lubridate",
 
 # This loads all packages and installs the package(from CRAN only) if not already in library.
 load_pkg(packagestoload)
+
+#maik branch
+
+#salut
+#test
